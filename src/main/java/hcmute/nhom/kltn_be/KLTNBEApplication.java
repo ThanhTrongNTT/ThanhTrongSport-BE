8cdb5031efaf198761e19f4f7ebefc37c4e98d36
ThanhTrong <trongnt-ts@hikesiya.co.jp>
2024-01-31 12:30:52 +0700

    First commit

---
+++ src/main/java/hcmute/nhom/kltn_be/KLTNBEApplication.java
@@ -0,0 +1,13 @@
+package hcmute.nhom.kltn_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KLTNBEApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KLTNBEApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hcmute/nhom/kltn_be/KltnBeApplicationTests.java
@@ -0,0 +1,13 @@
+package hcmute.nhom.kltn_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KltnBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
