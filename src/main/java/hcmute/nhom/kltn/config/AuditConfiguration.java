d506ee798d4d0b1c232cfd9f3c47a0b4b4991321
ThanhTrong <trongnt-ts@hikesiya.co.jp>
2024-03-15 00:01:31 +0700

    Adding WebSecurity and Audit configuration

---
+++ src/main/java/hcmute/nhom/kltn/config/AuditConfiguration.java
@@ -0,0 +1,16 @@
+package hcmute.nhom.kltn.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ * Class AuditConfiguration.
+ *
+ * @author: ThanhTrong
+ * @function_id:
+ * @version:
+ **/
+@Configuration
+@EnableJpaAuditing
+public class AuditConfiguration {
+}
---
+++ src/main/java/hcmute/nhom/kltn/config/WebSecurityConfig.java
@@ -0,0 +1,83 @@
+package hcmute.nhom.kltn.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import hcmute.nhom.kltn.security.jwt.JwtEntryPoint;
+import hcmute.nhom.kltn.security.jwt.JwtTokenFilter;
+import hcmute.nhom.kltn.security.pricipal.CustomUserDetailService;
+
+/**
+ * Class WebSecurityConfig.
+ *
+ * @author: ThanhTrong
+ * @function_id:
+ * @version:
+ **/
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final CustomUserDetailService customUserDetailsService;
+    private final JwtEntryPoint jwtEntryPoint;
+
+    /**
+     * JwtTokenFilter.
+     *
+     * @return JwtTokenFilter
+     */
+    @Bean
+    public JwtTokenFilter jwtTokenFilter() {
+        return new JwtTokenFilter();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // Password encoder, để Spring Security sử dụng mã hóa mật khẩu người dùng
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        // Get AuthenticationManager bean
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.userDetailsService(customUserDetailsService) // Cung cấp userservice cho spring security
+                .passwordEncoder(passwordEncoder()); // cung cấp password encoder
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors() //Ngăn chặn truy cập từ các domain khác
+                .and()
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/v1/auth/**").permitAll()
+                .antMatchers("/api/v1/token/**").permitAll()
+                .anyRequest().authenticated()
+                .and().exceptionHandling()
+                .authenticationEntryPoint(jwtEntryPoint)
+                //Tất cả các request khác đều được phải xác thực trước khi truy cập
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        // Thêm một lớp Filter kiểm tra jwt
+        http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+}
