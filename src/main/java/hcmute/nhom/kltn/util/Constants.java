4465f30f88486d26c78c490a7ff4d6b958c5640c
ThanhTrong <trongnt-ts@hikesiya.co.jp>
2024-03-15 00:02:31 +0700

    Create Constants

---
+++ src/main/java/hcmute/nhom/kltn/util/Constants.java
@@ -0,0 +1,19 @@
+package hcmute.nhom.kltn.util;
+
+/**
+ * Class Constants.
+ *
+ * @author: ThanhTrong
+ * @function_id:
+ * @version:
+ **/
+public class Constants {
+    public static final String DEFAULT_PAGE_NUMBER = "0";
+    public static final String DEFAULT_PAGE_SIZE = "5";
+    public static final String DEFAULT_SORT_BY = "id";
+    public static final String DEFAULT_SORT_DIRECTION = "asc";
+    public static final int STAGE_LOGIN_SUCCESS = 1;
+    public static final boolean REMOVAL_FLAG_FALSE = false;
+    public static final boolean REMOVAL_FLAG_TRUE = true;
+
+}
---
+++ src/main/java/hcmute/nhom/kltn/util/SessionConstants.java
@@ -0,0 +1,17 @@
+package hcmute.nhom.kltn.util;
+
+/**
+ * Class SessionConstants.
+ *
+ * @author: ThanhTrong
+ * @function_id:
+ * @version:
+ **/
+public class SessionConstants {
+    public static final String IP_ADDRESS = "IP_ADDRESS";
+    public static final String USER_ID = "USER_ID";
+    public static final String USER_NAME = "USER_NAME";
+    public static final String USER_EMAIL = "USER_EMAIL";
+    public static final String LOGGED_IN = "LOGGED_IN";
+    public static final String ACTIVE = "ACTIVE";
+}
