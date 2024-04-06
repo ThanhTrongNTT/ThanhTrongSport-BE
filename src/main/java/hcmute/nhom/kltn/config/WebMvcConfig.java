6694fbf1ff5337ff01eb3a52181571c5c7ecaf47
ThanhTrong <trongnt-ts@hikesiya.co.jp>
2024-04-06 22:57:19 +0700

    Config WebMvc

---
+++ src/main/java/hcmute/nhom/kltn/config/WebMvcConfig.java
@@ -0,0 +1,31 @@
+package hcmute.nhom.kltn.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Class WebMvcConfig.
+ *
+ * @author: ThanhTrong
+ * @function_id:
+ * @version:
+ **/
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final long MAX_AGE_SECS = 3600;
+
+    //    @Value("${app.cors.allowedOrigins}")
+    //    private String[] allowedOrigins;
+    //    @Value("${app.cors.allowedOriginsAdmin}")
+    //    private String[] allowedOriginsAdmin;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4000","http://localhost:3000","http://localhost:3006")
+                .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
+                .maxAge(MAX_AGE_SECS);
+    }
+}
